5cf48474ac9f16e7da69b33e7f528476440a5d36
timing1018 <73340657+timing1018@users.noreply.github.com>
2023-07-04 18:15:51 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    return num % 2 == 0 ? "Even" : "Odd";
+}
+
+// function solution(num) {
+//     if(num % 2 == 0) {
+//         return "Even"
+//     } else {
+//         return 
+//     }
+// }
