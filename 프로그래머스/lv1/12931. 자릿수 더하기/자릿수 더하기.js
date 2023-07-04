628af8766e3d7a649c5613ca5c304616e697313b
timing1018 <73340657+timing1018@users.noreply.github.com>
2023-07-04 14:59:24 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,13 +1,25 @@
 function solution(n)
 {
     var answer = 0;
-    const str = String(n);
-    const mapfn = (arg) => Number(arg);
-    const newArr = str.split('').map(mapfn);
+    n = n + ""; // 문자열 변환
     
-    for(let i = 0; i < newArr.length; i++) {
-        answer += newArr[i];
+    for(let i = 0; i < n.length; i++) {
+        answer += Number(n[i]);
     }
 
     return answer;
 }
+
+// function solution(n)
+// {
+//     var answer = 0;
+//     const str = String(n);
+//     const mapfn = (arg) => Number(arg);
+//     const newArr = str.split('').map(mapfn);
+    
+//     for(let i = 0; i < newArr.length; i++) {
+//         answer += newArr[i];
+//     }
+
+//     return answer;
+// }
